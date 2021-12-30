a76ce91d9ee882098deb85dae62337b9ae69a664
satokaiba <96111103+satokaiba@users.noreply.github.com>
2021-12-30 17:17:57 -0600

    wave w message

--- src/App.js
+++ src/App.js
@@ -4,10 +4,55 @@ import './App.css';
 import abi from './utils/WavePortal.json';
 
 const App = () => {
-  const [currentAccount, setCurrentAccount] = useState("");
-  const contractAddress = "0xc5AFef84CC571a0228C0C057cCC98eCcfA8A1811";
+  const contractAddress = "0xb02aB560aFB80Ebc3D3f5dCC71D5075a76589cD0";
   const contractABI = abi.abi;
+  const [currentAccount, setCurrentAccount] = useState("");
+  /*
+   * All state property to store all waves
+   */
+  const [allWaves, setAllWaves] = useState([]);
 
+  /*
+   * Create a method that gets all waves from your contract
+   */
+  const getAllWaves = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        /*
+         * Call the getAllWaves method from your Smart Contract
+         */
+        const waves = await wavePortalContract.getAllWaves();
+
+
+        /*
+         * We only need address, timestamp, and message in our UI so let's
+         * pick those out
+         */
+        let wavesCleaned = [];
+        waves.forEach(wave => {
+          wavesCleaned.push({
+            address: wave.waver,
+            timestamp: new Date(wave.timestamp * 1000),
+            message: wave.message
+          });
+        });
+
+        /*
+         * Store our data in React State
+         */
+        setAllWaves(wavesCleaned);
+      } else {
+        console.log("Ethereum object doesn't exist!")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -28,6 +73,7 @@ const App = () => {
         console.log("Found an authorized account:", account);
         userAddyHere.innerText = account;
         setCurrentAccount(account);
+        getAllWaves();
       } else {
         console.log("No authorized account found")
       }
@@ -73,7 +119,8 @@ const App = () => {
         /*
         * Execute the actual wave from your smart contract
         */
-        const waveTxn = await wavePortalContract.wave();
+        let theMsg = (!document.querySelector("#msg").value) ? "gm" : document.querySelector("#msg").value;
+        const waveTxn = await wavePortalContract.wave(theMsg);
         console.log("Mining...", waveTxn.hash);
 
         await waveTxn.wait();
@@ -89,27 +136,6 @@ const App = () => {
     }
   }
 
-  const checkwave = async () => {
-    try {
-      const { ethereum } = window;
-      const provider = new ethers.providers.Web3Provider(ethereum);
-      const signer = provider.getSigner();
-      const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
-      const countButton = document.querySelector('.chkButton');
-      let count = await wavePortalContract.getTotalWaves();
-      let buttonString = "";
-
-      if (!ethereum) { console.log('NOTE: no ethereum object!') }
-      else {
-        buttonString = "Total waves: " + count.toNumber();
-        countButton.innerText = buttonString;
-      }
-    }
-    catch (error) {
-      alert(error);
-    }
-  }
-
   useEffect(() => {
     checkIfWalletIsConnected();
   }, [])
@@ -118,31 +144,38 @@ const App = () => {
     <div className="mainContainer">
       <div className="dataContainer">
         <div className="header">
-        👋 Hey there! </div>
+          <span role="img" aria-label="hand waving emoji">👋</span> Hey there!
+        </div>
         <div style={{display: 'flex', justifyContent: 'center'}}>
-        <div id="spotForAddy">Your Address: <span id="userAddr"></span>
-        </div></div>
-
-        <div className="bio">
-          I am sato kaiba and this is my wave machine! Built on Rinkeby thanks to <a href="https://buildspace.so/" id='bs-link'>Buildspace!</a>
+          <div id="spotForAddy">Your Address: <span id="userAddr"></span></div>
         </div>
 
+        <div className="bio">
+        I am sato kaiba and this is my wave machine! Built on Rinkeby thanks to <a href="https://buildspace.so/" id='bs-link'>Buildspace!</a>
+        </div>
+
+        <div className="bio" style={{marginTop: 4}}>Write something in the box below and...</div>
+
+        <input id="msg" placeholder="gm"></input>
+
         <button className="waveButton" onClick={wave}>
-          Wave at Me
+          Wave &amp; Message Me!
         </button>
 
-        <button className="waveButton chkButton" style={{cursor:`zoom-in`}} onClick={checkwave}>
-        How many waves?
-        </button>
-
-        {/*
-        * If there is no currentAccount render this button
-        */}
         {!currentAccount && (
           <button className="waveButton" onClick={connectWallet}>
             Connect Wallet
           </button>
         )}
+
+        {allWaves.map((wave, index) => {
+          return (
+            <div key={index} style={{ backgroundColor: "gray", marginTop: "16px", padding: "8px" }}>
+              <div>Address: {wave.address}</div>
+              <div>Time: {wave.timestamp.toString()}</div>
+              <div>Message: {wave.message}</div>
+            </div>)
+        })}
       </div>
     </div>
   );
